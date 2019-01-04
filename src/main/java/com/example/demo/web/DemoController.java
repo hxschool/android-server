fa4dbf14740a237edd0d537d0af840086b24eeb0
zhaojunfei <773152@qq.com>
2019-01-04 14:41:02 +0800

    支持json格式返回

--- src/main/java/com/example/demo/web/DemoController.java
+++ src/main/java/com/example/demo/web/DemoController.java
@@ -7,10 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DemoController {
 	@RequestMapping("demo")
+	@ResponseBody
 	public String demo(HttpServletRequest request) throws IOException {
 		String str = IOUtils.toString(request.getInputStream());
 		System.out.println(str);
